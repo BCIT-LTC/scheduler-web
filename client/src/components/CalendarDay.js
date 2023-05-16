21a2b3e55fd2dd540e5965ccf23b82b6a8a7dc93
Sam(Gangmin) Bae <sbae000420@gmail.com>
2023-05-16 14:53:35 -0700

    Revert "Needed to comment out data to show the calendar"
    This reverts commit 1185216e2e6424e076bf1b2540f6069f58b23da8.

--- client/src/components/CalendarDay.js
+++ client/src/components/CalendarDay.js
@@ -1,7 +1,8 @@
-import "./calendar-day.css";
+import "./calendar-day.css"
 
-export default function CalendarDay({ date, data }) {
-    /*
+export default function CalendarDay ({ date, data }) {
+
+  /*
   day:3
   end time: 0.4791666666666667
   facilitator: "Jasica"
@@ -10,14 +11,18 @@ export default function CalendarDay({ date, data }) {
   start time: 0.3541666666666667
   __rowNum__: 1
   */
-    //console.log("what is data", data, date)
-    return (
-        <div className="calendar-day">
-            {data ? (
-                <div className="lab">{/* {data} */}</div>
-            ) : (
-                <div className="no-lab" />
-            )}
-        </div>
-    );
+//console.log("what is data", data, date)
+  return (
+    <div className="calendar-day">
+      { data ? (
+        <div className="lab">
+          {data}
+          
+          </div>
+        )
+      : (
+        <div className="no-lab" />
+      )}
+    </div>
+  )
 }
