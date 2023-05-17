4fb2301a8e0ae277c579cf2a02bab8296a98560c
Sam Bredenhof <sambredenhof@gmail.com>
2023-05-16 18:01:00 -0700

    fix bug and some formatting

--- client/src/components/CalendarDay.js
+++ client/src/components/CalendarDay.js
@@ -1,28 +1,17 @@
 import "./calendar-day.css"
 
-export default function CalendarDay ({ date, data }) {
+export default function CalendarDay({ date, data }) {
 
-  /*
-  day:3
-  end time: 0.4791666666666667
-  facilitator: "Jasica"
-  month: "feb"
-  room number: 3020
-  start time: 0.3541666666666667
-  __rowNum__: 1
-  */
-//console.log("what is data", data, date)
   return (
     <div className="calendar-day">
-      { data ? (
-        <div className="lab">
-          {data}
-          
-          </div>
-        )
-      : (
-        <div className="no-lab" />
-      )}
+      {data ? (
+        <div>
+          <div>{data['start_time']}-{data['end_time']}</div>
+        </div>
+      )
+        : (
+          <div className="no-lab" />
+        )}
     </div>
   )
 }
