305dc4c1065e9c8542a92586f15f1a4be34ce729
Andy Tran <AyyyTran@gmail.com>
2023-05-23 01:34:24 -0700

    updated route js documentation

--- routes/contact.js
+++ routes/contact.js
@@ -5,18 +5,42 @@ const router = express.Router();
 let contactMessage =
   'For any questions related to the Open Lab Application or general use of Open Lab, please contact jasica_munday@BCIT.ca. For any other inquiries (e.g. clinical skills) please contact your clinical instructor.';
 
-// GET contact message
+/**
+ * GET contact message
+ * @name get/contact
+ * @function
+ * @memberof module:routers/contact~router
+ * @inner
+ *
+ * @param {Object} req - The request object
+ * @param {Object} res - The response object
+ *
+ * @returns {Object} - JSON object with the contact message
+ */
 router.get('/contact', (req, res) => {
+  // Responds with the current contact message as JSON
   res.json({message: contactMessage});
 });
 
-// POST contact message
+/**
+ * POST contact message
+ * @name post/contact
+ * @function
+ * @memberof module:routers/contact~router
+ * @inner
+ *
+ * @param {Object} req - The request object
+ * @param {Object} res - The response object
+ *
+ * @returns {Object} - JSON object with the success message
+ */
 router.post('/contact', (req, res) => {
   const {message} = req.body;
 
-  // Update the contact message
+  // Update the contact message with the new message received in the request body
   contactMessage = message;
 
+  // Responds with a JSON object indicating the success and a message
   res.json({success: true, message: 'Contact message updated successfully.'});
 });
 
--- routes/indexRoute.js
+++ routes/indexRoute.js
@@ -1,10 +1,23 @@
-require("dotenv").config();
-const express = require("express");
+require('dotenv').config();
+const express = require('express');
 const router = express.Router();
-const path = require("path");
+const path = require('path');
 
-router.get("*", (req, res) => {
-  res.sendFile(path.resolve(__dirname, "../client/build/index.html"));
+/**
+ * Route to serve the client application
+ * @name get/*
+ * @function
+ * @memberof module:routers/mainRouter~router
+ * @inner
+ *
+ * @param {Object} req - The request object
+ * @param {Object} res - The response object
+ *
+ * @returns {Object} - The client application HTML file
+ */
+router.get('*', (req, res) => {
+  // Resolves the absolute path to the client application's HTML file
+  res.sendFile(path.resolve(__dirname, '../client/build/index.html'));
 });
 
 module.exports = router;
