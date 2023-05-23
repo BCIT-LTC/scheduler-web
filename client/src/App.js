f19e261a8c576501e970171734291d55bcf71f02
Dennis Kim <dkim334@my.bcit.ca>
2023-05-22 20:07:32 -0700

    announcements made available to students wo CUD

--- client/src/App.js
+++ client/src/App.js
@@ -1,11 +1,11 @@
-import {useEffect, useContext, useState} from 'react';
-import './App.css';
-import 'react-calendar/dist/Calendar.css';
-import Login from './components/Login/Login.js';
-import logout from './containers/logout';
-import {Routes, Route, Link} from 'react-router-dom';
-import CalendarPage from './containers/CalendarPage';
-import Locallogin from './components/Locallogin/Locallogin';
+import { useEffect, useContext, useState } from "react";
+import "./App.css";
+import "react-calendar/dist/Calendar.css";
+import Login from "./components/Login/Login.js";
+import logout from "./containers/logout";
+import { Routes, Route, Link } from "react-router-dom";
+import CalendarPage from "./containers/CalendarPage";
+import Locallogin from "./components/Locallogin/Locallogin";
 import DataForm from "./containers/DataForm";
 import SurveyPage from "./containers/SurveyPage";
 import OpenLabSignIn from "./containers/OpenLabSignIn";
@@ -13,14 +13,14 @@ import Home from "./containers/home";
 import DropdownAnnouncement from "./components/Announcement_dropdown/announcement.js";
 import Announcement from "./components/Announcements/Announcement";
 import Faq from "./components/Faq/Faq";
-import Cookies from 'js-cookie';
-import jwtDecode from 'jwt-decode';
+import Cookies from "js-cookie";
+import jwtDecode from "jwt-decode";
 import AdminList from "./components/AdminList/AdminList";
 import LabGuidelinesPage from "./containers/LabGuidelinesPage";
 
 function App() {
   const [showLocalLogin, setShowLocalLogin] = useState();
-  let jwt = Cookies.get('jwt');
+  let jwt = Cookies.get("jwt");
   let user = null;
   if (jwt !== undefined) {
     user = jwtDecode(jwt);
@@ -36,18 +36,32 @@ function App() {
     <>
       <nav className="navbar">
         <div className="nav-left">
-          <Link to="/"><img src="bcit_logo.png" className="bcit-logo" alt="BCIT logo" /></Link>
+          <Link to="/">
+            <img src="bcit_logo.png" className="bcit-logo" alt="BCIT logo" />
+          </Link>
         </div>
         <div id="navIcon">
           <div className="nav-icon">
             <DropdownAnnouncement />
           </div>
-          {
-            user.isLocal !== undefined ?
-              <Link to="/admins" className="nav-icon"><img src="./gear.svg" className="home-logo filter-blue" alt="settings" /></Link>
-              : ''
-          }
-          <input type="image" src="./logout.svg" className="nav-icon logout-button filter-blue" alt="settings" onClick={logout} />
+          {user.isLocal !== undefined ? (
+            <Link to="/admins" className="nav-icon">
+              <img
+                src="./gear.svg"
+                className="home-logo filter-blue"
+                alt="settings"
+              />
+            </Link>
+          ) : (
+            ""
+          )}
+          <input
+            type="image"
+            src="./logout.svg"
+            className="nav-icon logout-button filter-blue"
+            alt="settings"
+            onClick={logout}
+          />
         </div>
       </nav>
       <Routes>
@@ -56,13 +70,11 @@ function App() {
         <Route path="/openlabs" element={<CalendarPage />} />
         <Route path="/openlabsignin" element={<OpenLabSignIn />} />
         <Route path="/survey" element={<SurveyPage />} />
-        < Route path="/guidelines" element={<LabGuidelinesPage />} />
+        <Route path="/guidelines" element={<LabGuidelinesPage />} />
         <Route path="/faq" element={<Faq />} />
 
         {user.isAdmin && <Route path="/update" element={<DataForm />} />}
-        {user.isAdmin && (
-          <Route path="/announcements" element={<Announcement />} />
-        )}
+        <Route path="/announcements" element={<Announcement />} />
         {user.isLocal && <Route path="/admins" element={<AdminList />} />}
       </Routes>
     </>
--- client/src/components/Announcements/AnnouncementTable.js
+++ client/src/components/Announcements/AnnouncementTable.js
@@ -1,16 +1,22 @@
 import { useState, useEffect } from "react";
 import Alert from "../Alert";
 import Cookies from "js-cookie";
+import jwtDecode from "jwt-decode";
 import "./Announcements.css";
 
 const AnnouncementTable = () => {
   const [table, setTable] = useState([]);
+  // when the user clicks on a button, deleteClicked is updated
   const [deleteClicked, setDeleteClicked] = useState({
     isOpen: false,
+    // idx stores the row of the announcement that the delete button belongs to
     idx: -1,
-  }); // when the user clicks on a button, deleteClicked is updated
-  const [editClicked, setEditClicked] = useState({ isOpen: false, idx: -1 }); // when the user clicks on a button, editClicked is updated
-  // idx stores the row of the announcement that the delete button belongs to
+  });
+  // when the user clicks on a button, editClicked is updated
+  const [editClicked, setEditClicked] = useState({ isOpen: false, idx: -1 });
+
+  const user = jwtDecode(Cookies.get("jwt"));
+  const isAdmins = user.isAdmin;
 
   useEffect(() => {
     const interval = setInterval(() => {
@@ -89,110 +95,142 @@ const AnnouncementTable = () => {
     }
   }
 
-  // the map function is used below to iterate through each announcement in the database and append it to a row, with the title, description, date, and delete button
-
-  //the position for the delete button is set to relative so that it does not dissapear when the Alert component appears
-
   return (
     <div>
       <div>
-        <table>
-          <thead>
-            <tr>
-              <th>Title</th>
-              <th>Description</th>
-              <th>Date</th>
-              <th>Edit</th>
-              <th>Delete</th>
-            </tr>
-          </thead>
-          <tbody>
-            {table.map((row, index) => (
-              <tr key={row.announcements_id} className="table-item">
-                <td>{row.title}</td>
-                <td className="text-overflow">{row.description}</td>
-                <td>
-                  {" "}
-                  {new Date(row.date).toLocaleString("en-US", {
-                    year: "numeric",
-                    month: "short",
-                    day: "numeric",
-                    hour: "numeric",
-                    minute: "numeric",
-                    hour12: true,
-                    timeZone: "America/Los_Angeles",
-                  })}
-                </td>
-
-                {/* WIP edit button */}
-                <td width="20%" style={{ position: "relative" }}>
-                  <button
-                    className="button"
-                    id="edit-button"
-                    type="submit"
-                    onClick={() => {
-                      setEditClicked({ isOpen: true, idx: index });
-                    }}
-                  >
-                    Edit
-                  </button>
-                  {editClicked.isOpen && editClicked.idx === index ? (
-                    <div style={{ position: "absolute", top: "100%", left: 0 }}>
-                      <Alert
-                        isOpen={true}
-                        popupType={"edit"}
-                        onClose={() =>
-                          setEditClicked({ isOpen: false, idx: -1 })
-                        }
-                        title="Edit Announcement"
-                        description="Make your changes to the announcement below and click 'Save' to save your changes."
-                        announcementTitle={row.title}
-                        announcementDescription={row.description}
-                        confirmBtnLabel="Save"
-                        onConfirm={(updatedTitle, updatedDescription) =>
-                          editAnnouncement(
-                            row.announcements_id,
-                            updatedTitle,
-                            updatedDescription
-                          )
-                        }
-                      />
-                    </div>
-                  ) : null}
-                </td>
-
-                <td width="20%" style={{ position: "relative" }}>
-                  <button
-                    className="button"
-                    id="delete-button"
-                    type="submit"
-                    onClick={() => {
-                      setDeleteClicked({ isOpen: true, idx: index });
-                    }}
-                  >
-                    Delete
-                  </button>
-                  {deleteClicked.isOpen && deleteClicked.idx === index ? (
-                    <div style={{ position: "absolute", top: "100%", left: 0 }}>
-                      <Alert
-                        isOpen={true}
-                        onClose={() =>
-                          setDeleteClicked({ isOpen: false, idx: -1 })
-                        }
-                        title="Delete Announcement"
-                        description="Are you sure you want to delete this?"
-                        confirmBtnLabel="Delete"
-                        onConfirm={() =>
-                          deleteAnnouncement(row.announcements_id)
-                        }
-                      />
-                    </div>
-                  ) : null}
-                </td>
+        {isAdmins && (
+          <table>
+            <thead>
+              <tr>
+                <th>Title</th>
+                <th>Description</th>
+                <th>Date</th>
+                <th>Edit</th>
+                <th>Delete</th>
               </tr>
-            ))}
-          </tbody>
-        </table>
+            </thead>
+            <tbody>
+              {table.map((row, index) => (
+                <tr key={row.announcements_id} className="table-item">
+                  <td>{row.title}</td>
+                  <td className="text-overflow">{row.description}</td>
+                  <td>
+                    {" "}
+                    {new Date(row.date).toLocaleString("en-US", {
+                      year: "numeric",
+                      month: "short",
+                      day: "numeric",
+                      hour: "numeric",
+                      minute: "numeric",
+                      hour12: true,
+                      timeZone: "America/Los_Angeles",
+                    })}
+                  </td>
+                  <td width="20%" style={{ position: "relative" }}>
+                    <button
+                      className="button"
+                      id="edit-button"
+                      type="submit"
+                      onClick={() => {
+                        setEditClicked({ isOpen: true, idx: index });
+                      }}
+                    >
+                      Edit
+                    </button>
+                    {editClicked.isOpen && editClicked.idx === index ? (
+                      <div
+                        style={{ position: "absolute", top: "100%", left: 0 }}
+                      >
+                        <Alert
+                          isOpen={true}
+                          popupType={"edit"}
+                          onClose={() =>
+                            setEditClicked({ isOpen: false, idx: -1 })
+                          }
+                          title="Edit Announcement"
+                          description="Make your changes to the announcement below and click 'Save' to save your changes."
+                          announcementTitle={row.title}
+                          announcementDescription={row.description}
+                          confirmBtnLabel="Save"
+                          onConfirm={(updatedTitle, updatedDescription) =>
+                            editAnnouncement(
+                              row.announcements_id,
+                              updatedTitle,
+                              updatedDescription
+                            )
+                          }
+                        />
+                      </div>
+                    ) : null}
+                  </td>
+
+                  <td width="20%" style={{ position: "relative" }}>
+                    <button
+                      className="button"
+                      id="delete-button"
+                      type="submit"
+                      onClick={() => {
+                        setDeleteClicked({ isOpen: true, idx: index });
+                      }}
+                    >
+                      Delete
+                    </button>
+                    {deleteClicked.isOpen && deleteClicked.idx === index ? (
+                      <div
+                        style={{ position: "absolute", top: "100%", left: 0 }}
+                      >
+                        <Alert
+                          isOpen={true}
+                          onClose={() =>
+                            setDeleteClicked({ isOpen: false, idx: -1 })
+                          }
+                          title="Delete Announcement"
+                          description="Are you sure you want to delete this?"
+                          confirmBtnLabel="Delete"
+                          onConfirm={() =>
+                            deleteAnnouncement(row.announcements_id)
+                          }
+                        />
+                      </div>
+                    ) : null}
+                  </td>
+                </tr>
+              ))}
+            </tbody>
+          </table>
+        )}
+
+        {!isAdmins && (
+          <table>
+            <thead>
+              <tr>
+                <th>Title</th>
+                <th>Description</th>
+                <th>Date</th>
+              </tr>
+            </thead>
+            <tbody>
+              {table.map((row, index) => (
+                <tr key={row.announcements_id} className="table-item">
+                  <td>{row.title}</td>
+                  <td className="text-overflow">{row.description}</td>
+                  <td>
+                    {" "}
+                    {new Date(row.date).toLocaleString("en-US", {
+                      year: "numeric",
+                      month: "short",
+                      day: "numeric",
+                      hour: "numeric",
+                      minute: "numeric",
+                      hour12: true,
+                      timeZone: "America/Los_Angeles",
+                    })}
+                  </td>
+                </tr>
+              ))}
+            </tbody>
+          </table>
+        )}
       </div>
     </div>
   );
