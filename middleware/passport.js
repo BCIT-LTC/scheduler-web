54a0e4cc8184f3be73f20319701e0ea242b429f8
Viresh Soedhwa <vsoedhwa@bcit.ca>
2023-07-05 18:39:54 -0700

    use openidconnect

--- middleware/passport.js
+++ middleware/passport.js
@@ -1,5 +1,6 @@
 const passport = require('passport');
 var saml = require('passport-saml');
+var OpenIDConnectStrategy = require('passport-openidconnect');
 var fs = require('fs');
 const jwt = require("jsonwebtoken");
 
@@ -47,6 +48,53 @@ var samlStrategy = new saml.Strategy({
     return done(null, { token: jwtToken });
 });
 
+
+var oidcStrategy = new OpenIDConnectStrategy.Strategy({
+    issuer: 'https://vault.ltc.bcit.ca/v1/identity/oidc/provider/ltc-auth',
+    authorizationURL: 'https://vault.ltc.bcit.ca/ui/vault/identity/oidc/provider/ltc-auth/authorize',
+    tokenURL: 'https://vault.ltc.bcit.ca/v1/identity/oidc/provider/ltc-auth/token',
+    userInfoURL: 'https://vault.ltc.bcit.ca/v1/identity/oidc/provider/ltc-auth/userinfo',
+    clientID: process.env['CLIENT_ID'],
+    clientSecret: process.env['CLIENT_SECRET'],
+    callbackURL: 'http://localhost:9000/login/callback'
+  }, async (profile, done) => {
+    console.log("profile info: ");
+    console.log("email:", profile.email);
+    console.log("firstname:", profile.firstname);
+    console.log("lastname:", profile.lastname);
+    console.log("(type):", profile.type);
+    console.log("(program):", profile.program);
+
+    let email = profile.email
+    let firstname = profile.firstname
+    let lastname = profile.lastname
+    let eligibleAdmin = (profile.program === 'BSN' && profile.type != 'student');
+    let jwtToken = jwt.sign({ email, firstname, lastname, eligibleAdmin }, process.env.SECRET_KEY);
+    await fetch(`${process.env.API_URL}login`, {
+        method: 'GET',
+        headers: {
+            'Authorization': `Bearer ${jwtToken}`,
+            'content-Type': 'application/json',
+        },
+        mode: 'cors',
+    }).then(async (response) => {
+        return await response.json();
+    }).then((response) => {
+        console.log(response);
+        let isAdmin = response;
+        jwtToken = jwt.sign({ email, firstname, lastname, isAdmin }, process.env.SECRET_KEY);
+        console.log(jwtToken)
+    });
+    return done(null, { token: jwtToken });
+});
+
+
+
+
+
+
+
+passport.use("oidcStrategy", oidcStrategy)
 passport.use("samlStrategy", samlStrategy)
 
 passport.serializeUser((user, done) => {
--- routes/openid_auth.js
+++ routes/openid_auth.js
@@ -27,24 +27,18 @@ const issuer = require('openid-client')
  * @function
  * @memberof module:routers/login~loginRouter
  */
-// router.post('/',
-//     passport.authenticate('samlStrategy', { failureRedirect: './', failureMessage: true }),
-//     function (req, res) {
-//         res.cookie('jwt', req.user.token, { httpOnly: false });
-//         console.log(`test redirect baseurl`)
-//         console.log(req.originalUrl)
-//         console.log(req.baseUrl)
-//         console.log(req.url)
-//         console.log(req.headers)
-//         console.log(req.path)
-//         console.log(req.hostname)
-//         res.redirect('/');
-//     });
-router.post("/", async (req, res) => {
-    console.log("cehck check check")
-    // console.log(issuer.Issuer)
-    const googleIssuer = await issuer.Issuer.discover('http://vault.ltc.bcit.ca/v1/identity/oidc/provider/ltc-auth/.well-known/keys');
-    console.log('Discovered issuer %s %O', googleIssuer.issuer, googleIssuer.metadata);
-});
+router.post('/',
+    passport.authenticate('oidcStrategy', { failureRedirect: './', failureMessage: true }),
+    function (req, res) {
+        res.cookie('jwt', req.user.token, { httpOnly: false });
+        console.log(`test redirect baseurl`)
+        console.log(req.originalUrl)
+        console.log(req.baseUrl)
+        console.log(req.url)
+        console.log(req.headers)
+        console.log(req.path)
+        console.log(req.hostname)
+        res.redirect('/');
+    });
 
 module.exports = router;
