a3c33e38ee8e5ecb4bd4482aff2d09032b37566b
Viresh Soedhwa <vsoedhwa@bcit.ca>
2023-09-11 10:59:55 -0700

    update deployment files

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -4,7 +4,7 @@ set -e
 
 # Get secrets from Vault init container (latest/stable environments only) and set as ENV VARS
 #
-if [ -f "/vault/secrets/config" ]; then grep -v -e '^#' -e '^[[:space:]]*$' /vault/secrets/config > .env; 
+if [ -f "/vault/secrets/config" ]; then grep -v -e '^#' -e '^[[:space:]]*$' /vault/secrets/config >> .env; 
 set -o allexport && source .env && set +o allexport;fi
 
 
@@ -18,15 +18,18 @@ fi
 
 # Inject SAML IdP certificate secret and manipulate to meet passport.js requirements
 #
-if [ -f "/vault/secrets/saml-idp-certificate.crt" ]; then
+if [ -n "${SAML_IDP_CERTIFICATE}" ]; then
+    echo "Review branch SAML IdP certificate injection..."
+    echo -e "\nSAML_IDP_CERT_STRING=$(echo "${SAML_IDP_CERTIFICATE}" | sed 1d | sed '$ d' | awk 'NF {sub(/\r/, ""); printf "%s\\n",$0;}' | sed 's/\\n//g')" >> .env
+elif [ -f "/vault/secrets/saml-idp-certificate.crt" ]; then
     echo "Injecting SAML IdP certificate from Vault..."
-    export SAML_IDP_CERT_STRING=$(cat /vault/secrets/saml-idp-certificate.crt | sed 1d | sed '$ d' | awk 'NF {sub(/\r/, ""); printf "%s\\n",$0;}' | sed 's/\\n//g')
-elif [ -f "/vault/secrets/bcit-saml-idp-certificate.crt" ]; then
-    echo "Injecting BCIT SAML IdP certificate from Vault..."
-    export SAML_IDP_CERT_STRING=$(cat /vault/secrets/bcit-saml-idp-certificate.crt | sed 1d | sed '$ d' | awk 'NF {sub(/\r/, ""); printf "%s\\n",$0;}' | sed 's/\\n//g')
+    echo -e "\nSAML_IDP_CERT_STRING=$(cat /vault/secrets/saml-idp-certificate.crt | sed 1d | sed '$ d' | awk 'NF {sub(/\r/, ""); printf "%s\\n",$0;}' | sed 's/\\n//g')" >> .env
+# elif [ -f "/vault/secrets/bcit-saml-idp-certificate.crt" ]; then
+#     echo "Injecting BCIT SAML IdP certificate from Vault..."
+#     echo -e "\nSAML_IDP_CERT_STRING=$(cat /vault/secrets/bcit-saml-idp-certificate.crt | sed 1d | sed '$ d' | awk 'NF {sub(/\r/, ""); printf "%s\\n",$0;}' | sed 's/\\n//g')" >> .env
 else
     echo "Vault secret doesn't exist...using default from /simplesaml/idp.crt instead..."
-    export SAML_IDP_CERT_STRING=$(cat /simplesaml/idp.crt | sed 1d | sed '$ d' | awk 'NF {sub(/\r/, ""); printf "%s\\n",$0;}' | sed 's/\\n//g')
+    echo -e "\nSAML_IDP_CERT_STRING=$(cat /simplesaml/idp.crt | sed 1d | sed '$ d' | awk 'NF {sub(/\r/, ""); printf "%s\\n",$0;}' | sed 's/\\n//g')" >> .env
 fi
 
 
