19301676dcb7f88ee8cdb106d1109cf651db306a
Kyle Hunter <kyle_hunter@bcit.ca>
2023-10-05 10:49:57 -0700

    updates local dev idp.cert mounting

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -11,32 +11,15 @@ if [ -z "${ADMIN_USERNAME}" ] || [ -z "${ADMIN_PASSWORD}" ] || [ -z "${JWT_AUTH_
 fi
 
 
-# Configure environment variables
-#
-#   - SAML_IDP_CERTIFICATE (required by passport.js) should be base64 encoded
-#
-if [ -n "${SAML_IDP_CERTIFICATE}" ]; then
-    printf "SAML_IDP_CERTIFICATE environment variable is set. Using it..."
-
-else
-    printf "SAML_IDP_CERTIFICATE environment variable is not set.\n\t Using the default certificate from /simplesaml/idp.crt instead..."
-    export SAML_IDP_CERTIFICATE=$(base64 -w 0 ./simplesaml/idp.crt)
-fi
-
-
 # If API_URL is set, use it. Otherwise, fall back to the default
 #
 if [ -n "${API_URL}" ]; then
     echo API_URL is set...setting API_URL to ${API_URL}
 else
     echo API_URL is not set. Using the default: https://latest--scheduler-api.ltc.bcit.ca/api/
-    export API_URL="https://latest--scheduler-api.ltc.bcit.ca/api/"
+    printf "API_URL=https://latest--scheduler-api.ltc.bcit.ca/api/" > .env
 fi
 
-
-# Store vars in `.env` so they can be picked-up by the app
-#
-printf "SAML_IDP_CERTIFICATE=%s\nAPI_URL=%s\n" "${SAML_IDP_CERTIFICATE}" "${API_URL}" > .env
 
 # Return to parent shell to run app
 #
--- middleware/passport.js
+++ middleware/passport.js
@@ -13,7 +13,7 @@ var samlStrategy = new saml.Strategy({
     audience: process.env.SAML_ISSUER,
     // wantAuthnResponseSigned: false,
     // signatureAlgorithm: 'sha256',
-    cert: process.env.SAML_IDP_CERTIFICATE,
+    cert: fs.readFileSync('/app/saml/idp.crt', 'utf-8'),
     // cert: process.env.SAML_IDP_CERT_STRING
     // privateKey: fs.readFileSync('saml/secrets/private.key', 'utf-8'),
     // passReqToCallback: true,
