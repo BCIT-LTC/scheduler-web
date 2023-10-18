3c249658808aa37d0e0b19eb04f8532ce8d15baa
KarlCue <kacue38@gmail.com>
2023-10-18 16:53:00 -0700

    added tests file with mock data

---
+++ client/src/tests/mock-data-user.js
@@ -0,0 +1,102 @@
+export const mockDataUser = [
+    {
+        "id": "1",
+        "first_name": "Henry",
+        "last_name": "Mathews",
+        "email": "henryM@bcit.ca",
+        "role": "student",
+        "school": "School of Health Sciences",
+        "program": "Bachelor of Science in Nursing",
+        "is_active": false
+    },
+    {
+        "id": "2",
+        "first_name": "Ira",
+        "last_name": "Whitehead",
+        "email": "iraW@bcit.ca",
+        "role": "student",
+        "school": "School of Health Sciences",
+        "program": "Bachelor of Science in Nursing",
+        "is_active": false
+    },
+    {
+        "id": "3",
+        "first_name": "Elizabeth",
+        "last_name": "Joyce",
+        "email": "E_joyce@bcit.ca",
+        "role": "admin",
+        "school": "School of Health Sciences",
+        "program": "Bachelor of Science in Nursing",
+        "is_active": false
+    },
+    {
+        "id": "4",
+        "first_name": "Jewel",
+        "last_name": "Silva",
+        "email": "JSilva@bcit.ca",
+        "role": "student",
+        "school": "School of Health Sciences",
+        "program": "Bachelor of Science in Nursing",
+        "is_active": false
+    },
+    {
+        "id": "5",
+        "first_name": "Angie",
+        "last_name": "Quinn",
+        "email": "AngieQ@bcit.ca",
+        "role": "admin",
+        "school": "School of Health Sciences",
+        "program": "Bachelor of Science in Nursing",
+        "is_active": false
+    },
+    {
+        "id": "6",
+        "first_name": "Jacques",
+        "last_name": "Villarreal",
+        "email": "JacVill@bcit.ca",
+        "role": "admin",
+        "school": "School of Health Sciences",
+        "program": "Bachelor of Science in Nursing",
+        "is_active": false
+    },
+    {
+        "id": "7",
+        "first_name": "Russel",
+        "last_name": "Mcintosh",
+        "email": "McRussel@bcit.ca",
+        "role": "student",
+        "school": "School of Health Sciences",
+        "program": "Bachelor of Science in Nursing",
+        "is_active": false
+    },
+    {
+        "id": "8",
+        "first_name": "Rodger",
+        "last_name": "Hall",
+        "email": "rhall@bcit.ca",
+        "role": "admin",
+        "school": "School of Health Sciences",
+        "program": "Bachelor of Science in Nursing",
+        "is_active": false
+    },
+    {
+        "id": "9",
+        "first_name": "Maribel",
+        "last_name": "Bartlett",
+        "email": "marbar@bcit.ca",
+        "role": "admin",
+        "school": "School of Health Sciences",
+        "program": "Bachelor of Science in Nursing",
+        "is_active": false
+    },
+    {
+        "id": "10",
+        "first_name": "Otto",
+        "last_name": "Schwartz",
+        "email": "bigShwartz@bcit.ca",
+        "role": "admin",
+        "school": "School of Health Sciences",
+        "program": "Bachelor of Science in Nursing",
+        "is_active": false
+    }                                        
+]
