414045cc0296a43caf9555e58ac4769f5143d2eb
KarlCue <kacue38@gmail.com>
2023-10-18 16:53:00 -0700

    added tests file with mock data

---
+++ client/src/tests/mock-data-anouncements.js
@@ -0,0 +1,62 @@
+export const mockDataAnouncements = [
+    {
+        "id": 1,
+        "title": "Welcome!",
+        "announcement": "Welcome to the BSN OpenLab Scheduler!",
+        "date": "2023-05-28T10:05:16.000Z",   
+      },
+      {
+        "id": 2,
+        "title": "Welcome!",
+        "announcement": "Welcome to the BSN OpenLab Scheduler!",
+        "date": "2023-06-28T10:05:16.000Z",   
+      },
+      {
+        "id": 3,
+        "title": "Welcome!",
+        "announcement": "Welcome to the BSN OpenLab Scheduler!",
+        "date": "2023-07-28T10:05:16.000Z",   
+      },
+      {
+        "id": 4,
+        "title": "Welcome!",
+        "announcement": "Welcome to the BSN OpenLab Scheduler!",
+        "date": "2023-08-28T10:05:16.000Z",   
+      },
+      {
+        "id": 5,
+        "title": "Welcome!",
+        "announcement": "Welcome to the BSN OpenLab Scheduler!",
+        "date": "2023-09-28T10:05:16.000Z",   
+      },
+      {
+        "id": 6,
+        "title": "Welcome!",
+        "announcement": "Welcome to the BSN OpenLab Scheduler!",
+        "date": "2023-10-28T10:05:16.000Z",   
+      },
+      {
+        "id": 7,
+        "title": "Welcome!",
+        "announcement": "Welcome to the BSN OpenLab Scheduler!",
+        "date": "2023-11-28T10:05:16.000Z",   
+      },
+      {
+        "id": 8,
+        "title": "Welcome!",
+        "announcement": "Welcome to the BSN OpenLab Scheduler!",
+        "date": "2023-12-28T10:05:16.000Z",   
+      },
+      {
+        "id": 9,
+        "title": "Welcome!",
+        "announcement": "Welcome to the BSN OpenLab Scheduler!",
+        "date": "2023-01-28T10:05:16.000Z",   
+      },
+      {
+        "id": 10,
+        "title": "Welcome!",
+        "announcement": "Welcome to the BSN OpenLab Scheduler!",
+        "date": "2023-02-28T10:05:16.000Z",   
+      }
+]
