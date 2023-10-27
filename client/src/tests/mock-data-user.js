49db6ce1fc99be59ac85710b711271ca601abd1b
KarlCue <kacue38@gmail.com>
2023-10-27 15:16:00 -0700

    made the data more unique

--- client/src/tests/mock-data-user.js
+++ client/src/tests/mock-data-user.js
@@ -14,7 +14,7 @@ export const mockDataUser = [
         "first_name": "Ira",
         "last_name": "Whitehead",
         "email": "iraW@bcit.ca",
-        "role": "student",
+        "role": "admin",
         "school": "School of Health Sciences",
         "program": "Bachelor of Science in Nursing",
         "is_active": false
@@ -24,7 +24,7 @@ export const mockDataUser = [
         "first_name": "Elizabeth",
         "last_name": "Joyce",
         "email": "E_joyce@bcit.ca",
-        "role": "admin",
+        "role": "instructor",
         "school": "School of Health Sciences",
         "program": "Bachelor of Science in Nursing",
         "is_active": false
@@ -54,7 +54,7 @@ export const mockDataUser = [
         "first_name": "Jacques",
         "last_name": "Villarreal",
         "email": "JacVill@bcit.ca",
-        "role": "admin",
+        "role": "instructor",
         "school": "School of Health Sciences",
         "program": "Bachelor of Science in Nursing",
         "is_active": false
@@ -84,7 +84,7 @@ export const mockDataUser = [
         "first_name": "Maribel",
         "last_name": "Bartlett",
         "email": "marbar@bcit.ca",
-        "role": "admin",
+        "role": "instructor",
         "school": "School of Health Sciences",
         "program": "Bachelor of Science in Nursing",
         "is_active": false
@@ -94,7 +94,7 @@ export const mockDataUser = [
         "first_name": "Otto",
         "last_name": "Schwartz",
         "email": "bigShwartz@bcit.ca",
-        "role": "admin",
+        "role": "student",
         "school": "School of Health Sciences",
         "program": "Bachelor of Science in Nursing",
         "is_active": false
