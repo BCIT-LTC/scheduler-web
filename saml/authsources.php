76cf6c1e2044809e850e6bd94bb38261a4664615
Viresh Soedhwa <vsoedhwa@bcit.ca>
2023-10-27 17:52:57 -0700

    fix: Resolve "update the outdated user json field for saml and local admin"

--- saml/authsources.php
+++ saml/authsources.php
@@ -10,24 +10,27 @@ $config = array(
         'exampleauth:UserPass',
         'admin@bcit.ca:admin' => array(
             'email' => 'admin@bcit.ca',
-            'firstname' => 'admin_firstname',
-            'lastname' => 'admin_lastname',
-            'type' => 'non-student', # in reality it could be "employee" or "instructor" or any other non student fields
-            'program' => 'BSN' # the exact name in the field is not know so use BSN for now
+            'first_name' => 'admin_firstname',
+            'last_name' => 'admin_lastname',
+            'role' => 'admin', 
+            'school' => 'School of Health Sciences',
+            'program' => 'Bachelor of Science in Nursing' 
         ),
         'instructor@bcit.ca:instructor' => array(
             'email' => 'instructor@bcit.ca',
-            'firstname' => 'instructor_firstname',
-            'lastname' => 'instructor_lastname',
-            'type' => 'non-student', # in reality it could be "employee" or "instructor" or any other non student fields
-            'program' => 'BSN' # the exact name in the field is not know so use BSN for now
+            'first_name' => 'instructor_firstname',
+            'last_name' => 'instructor_lastname',
+            'role' => 'instructor', 
+            'school' => 'School of Health Sciences',
+            'program' => 'Bachelor of Science in Nursing' 
         ),
         'student@bcit.ca:student' => array(
             'email' => 'student@bcit.ca',
-            'firstname' => 'student_firstname',
-            'lastname' => 'student_lastname',
-            'type' => 'student',
-            'program' => 'BSN'
+            'first_name' => 'student_firstname',
+            'last_name' => 'student_lastname',
+            'role' => 'student', 
+            'school' => 'School of Health Sciences',
+            'program' => 'Bachelor of Science in Nursing' 
         )
     ),
 
