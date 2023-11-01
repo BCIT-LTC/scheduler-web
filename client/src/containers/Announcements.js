725b4aaa412784fdaef8378a738dcdd18a049566
Amin <mdavari3@my.bcit.ca>
2023-11-01 01:44:33 -0700

    Revert "- Introduced `NewAnnouncementComponent` for adding, editing (deleting comes..."
    This reverts commit 4779a75a867b1f870783a6564b80c1e4161e5947

--- client/src/containers/Announcements.js
+++ client/src/containers/Announcements.js
@@ -1,58 +1,17 @@
-import React, { useState } from 'react';
-import Box from '@mui/material/Box';
-import List from '@mui/material/List';
-import ListItem from '@mui/material/ListItem';
-import ListItemButton from '@mui/material/ListItemButton';
-import ListItemText from '@mui/material/ListItemText';
-import Divider from '@mui/material/Divider';
-import Button from '@mui/material/Button';
-import NewAnnouncementComponent from '../components/Announcements/NewAnnouncementComponent';
-const AnnouncementPage = () => {
-        const [showNewAnnouncement, setShowNewAnnouncement] = useState(false);
-        const isUserInstructor = true; // This should be set based on the authentication/authorization logic
+import { useContext } from "react";
+import { Navigate } from "react-router-dom"
 
-        const handleNewAnnouncementClick = () => {
-            setShowNewAnnouncement(true);
-        };
-        return (
-            <Box
-                justifyContent="center"
-                alignItems="center"
-                minHeight="100vh"
-                sx={{width: '100%', maxWidth: 360, bgcolor: 'background.paper'}}>
-                <nav aria-label="main">
-                    <List
-                        sx={{width: '100%', maxWidth: 360, bgcolor: 'background.paper'}}
-                    >
-                        <ListItem disablePadding>
-                            <ListItemButton>
-                                <ListItemText primary="Lab open today"/>
-                            </ListItemButton>
-                        </ListItem>
-                        <Divider/>
-                        <ListItem disablePadding>
-                            <ListItemButton>
-                                <ListItemText primary="Lab flooded"/>
-                            </ListItemButton>
-                        </ListItem>
-                        <Divider/>
-                        <ListItem disablePadding>
-                            <ListItemButton>
-                                <ListItemText primary="Lab closed due to zombie infestation"/>
-                            </ListItemButton>
-                        </ListItem>
-                        <Divider/>
-                    </List>
-                </nav>
-                {isUserInstructor && (
-                    <Button variant="contained" color="primary" onClick={handleNewAnnouncementClick}>
-                        New Announcement
-                    </Button>
-                )}
+export default function Announcements() {
 
-                {showNewAnnouncement && <NewAnnouncementComponent/>}
-            </Box>
-        );
-    };
-
-export default AnnouncementPage;
+  return (
+    <>
+        TODO: announcements containers 
+        <br></br>
+        See: /src/components/Announcements/
+        <br></br>
+        edit the following files:
+        <br></br>
+        AnnouncementFilter.jsx and AnnouncementList.jsx
+    </>
+  );
+}
