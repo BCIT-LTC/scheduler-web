3fb72bf5b556cf2c2db5bd2c838dd669c0b58b66
Cloue <maczyepez@gmail.com>
2023-11-06 18:22:15 -0800

    Add Announcement filter and sort list

--- client/src/containers/Announcements.js
+++ client/src/containers/Announcements.js
@@ -1,17 +1,141 @@
-import { useContext } from "react";
-import { Navigate } from "react-router-dom"
+import React, { useState, useMemo } from 'react'; 
+import { Container, Grid, Paper, Typography, Box, Select, MenuItem, FormControl, InputLabel, Button  } from '@mui/material';
+import AnnouncementList from "../components/Announcements/AnnouncementList"
+import AnnouncementFilter from '../components/Announcements/AnnouncementFilter';
+const announcementsData = [
+    {
+      id: 1,
+      title: 'Blood pressure station offline for maintenance',
+      date: 'February 4, 2023',
+      description: 'Lorem Ipsum is simply dummy text...'
+    },
+    {
+      id: 1,
+      title: 'Blood pressure station offline for maintenance',
+      date: 'February 4, 2023',
+      description: 'Lorem Ipsum is simply dummy text...'
+    },
+    {
+      id: 1,
+      title: 'Blood pressure station offline for maintenance',
+      date: 'February 4, 2023',
+      description: 'Lorem Ipsum is simply dummy text...'
+    },
+    {
+      id: 1,
+      title: 'Blood pressure station offline for maintenance',
+      date: 'February 4, 2023',
+      description: 'Lorem Ipsum is simply dummy text...'
+    },
+    // ...other announcements
+];
 
-export default function Announcements() {
 
+const Announcements = () => {
+  const [filters, setFilters] = useState({
+    date: null,
+    search: '',
+    rooms: [],
+    sort: 'latest',
+  });
+
+  const handleSortChange = (event) => {
+    setFilters(prev => ({ ...prev, sort: event.target.value }));
+  };
+
+  const sortedAnnouncements = useMemo(() => {
+    return [...announcementsData].sort((a, b) => {
+      if (filters.sort === 'latest') {
+        return new Date(b.date) - new Date(a.date);
+      } else {
+        return new Date(a.date) - new Date(b.date);
+      }
+    });
+  }, [announcementsData, filters.sort]);
+
+
+
+  const handleFilterChange = (filterType, value) => {
+    setFilters(prev => ({ ...prev, [filterType]: value }));
+  };
+
+  const handleSearchChange = (value) => {
+    setFilters(prev => ({ ...prev, search: value }));
+  };
+  
+  const filteredAnnouncements = sortedAnnouncements.filter(announcement => {
+    // Filter by date if a date is set
+    if (filters.date && announcement.date !== filters.date) {
+      return false;
+    }
+    // Filter by search text if search text is provided
+    if (filters.search && !announcement.title.toLowerCase().includes(filters.search.toLowerCase())) {
+      return false;
+    }
+    // Filter by selected rooms if any rooms are selected
+    if (filters.rooms.length > 0 && !filters.rooms.includes(announcement.room)) {
+      return false;
+    }
+    return true;
+  });
+  
+  const TitleBar = () => (
+    <Box sx={{ 
+      bgcolor: 'white', 
+      color: 'black', 
+      p: 2,
+      display: 'flex',
+      justifyContent: 'space-between',
+      alignItems: 'center',
+      borderBottom: '1px solid #e0e0e0' 
+    }}>
+        <Typography variant="h6">Announcements</Typography>
+        <Box sx={{ display: 'flex', alignItems: 'center' }}>
+            <FormControl variant="outlined" size="small" sx={{ mr: 1 }}>
+                <InputLabel id="sort-label" sx={{ color: 'inherit' }}>Sort</InputLabel>
+                <Select
+                    labelId="sort-label"
+                    id="sort-select"
+                    value={filters.sort}
+                    label="Sort"
+                    onChange={handleSortChange}
+                    sx={{ color: 'black', background: 'white', '& .MuiOutlinedInput-notchedOutline': { borderColor: 'white' } }}
+                >
+                    <MenuItem value="latest">Latest</MenuItem>
+                    <MenuItem value="oldest">Oldest</MenuItem>
+                </Select>
+            </FormControl>
+            <Button 
+                variant="contained" 
+                sx={{ 
+                    bgcolor: '#1976d2', 
+                    color: 'white', 
+                    '&:hover': { bgcolor: '#1565c0' } 
+                }}
+            >
+                NEW
+            </Button>
+        </Box>
+    </Box>
+);
   return (
-    <>
-        TODO: announcements containers 
-        <br></br>
-        See: /src/components/Announcements/
-        <br></br>
-        edit the following files:
-        <br></br>
-        AnnouncementFilter.jsx and AnnouncementList.jsx
-    </>
-  );
-}
+    <Container maxWidth="lg" sx={{ mt: 4, mb: 4 }}>
+        <Paper elevation={3} sx={{ p: 2, boxShadow: 1, borderRadius: 2 }}>
+            <TitleBar />
+            <Grid container spacing={2}> 
+                <Grid item xs={12} md={4}>
+                    <AnnouncementFilter 
+                        onSearchChange={handleSearchChange} 
+                        onFilterChange={handleFilterChange} 
+                    />
+                </Grid>
+                <Grid item xs={12} md={8}>
+                    <AnnouncementList announcements={filteredAnnouncements} />
+                </Grid>
+            </Grid>
+        </Paper>
+    </Container>
+  );    
+};
+
+export default Announcements;
