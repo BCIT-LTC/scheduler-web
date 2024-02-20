08e29f4ab44a06b55bfecf360b7248ce3c04a5be
Brandon Chan <brandonchan45@gmail.com>
2024-02-20 13:05:28 -0800

    refactored api_requests, removed API_caller.js

--- routes/api.js
+++ routes/api.js
@@ -1,15 +1,24 @@
 const express = require("express");
 
 const router = express.Router();
-const callAPI = require("../services/API_caller");
+const ApiRequests = require("../services/api_requests");
 
-router.all("*", async (req, res) => {
+router.all("/*", async (req, res) => {
   console.log("Accessing the API...");
+
+  const api_requests = new ApiRequests(
+    req.path,
+    req.headers.authorization,
+    req.method,
+    req.body
+  );
+
   try {
-    let response = callAPI(req.path, req.method, req);
-    res.status(response.status).json(response.data);
+    let data = api_requests.all();
+    console.log("Data: ", data);
+    res.status(data.status).json(data);
   } catch (error) {
-    console.log(error);
+    console.log("Error in API call: ", error);
     return error;
   }
 });
--- services/api_requests.js
+++ services/api_requests.js
@@ -4,55 +4,41 @@
  * @classdesc This file contains the class ApiRequests. This class is used to make requests to the API.
  */
 module.exports = class ApiRequests {
-    constructor(path, auth_token, body) {
-        if (path === undefined) {
-            throw new Error("ApiRequests path is undefined");
-        }
-        if (auth_token === undefined) {
-            throw new Error("ApiRequest auth_token is undefined");
-        }
-        this.url = new URL(process.env.API_URL + path)
-        const headers = new Headers();
-        headers.append("Content-Type", "application/json");
-        headers.append("Authorization", auth_token);
-        this.fetchoptions = {
-            headers: headers,
-            mode: "cors",
-            body: body
-        };
+  constructor(path, auth_token, method, body) {
+    if (path === undefined) {
+      throw new Error("ApiRequests path is undefined");
     }
+    if (auth_token === undefined) {
+      throw new Error("ApiRequest auth_token is undefined");
+    }
+    this.url = new URL(process.env.API_URL + path);
+    const headers = new Headers();
+    headers.append("Content-Type", "application/json");
+    headers.append("Authorization", auth_token);
+    this.fetchoptions = {
+      method: method,
+      headers: headers,
+      mode: "cors",
+      body: body,
+    };
+  }
 
-    /**
-     * This file contains the function used to call APIs on the serverside.
-     * It works in conjunction with the client side API routes.
-     *
-     * @returns {<{data: {error}, status: number}>}
-     */
-    async get() {
-        this.fetchoptions.method = "GET";
-        let data;
-        try {
-            let response = await fetch(this.url, this.fetchoptions)
-            data = await response.json();
-            return data;
-        } catch (error) {
-            console.log(error);
-            return error;
-        }
+  /**
+   * This file contains the function used to call APIs on the serverside.
+   * It works in conjunction with the client side API routes.
+   * Will GET, POST, PUT, DELETE to the API.
+   *
+   * @returns {<{data: {error}, status: number}>}
+   */
+  async all() {
+    let data;
+    try {
+      let response = await fetch(this.url, this.fetchoptions);
+      data = await response.json();
+      return data;
+    } catch (error) {
+      console.log(error);
+      return error;
     }
-
-    // used to create new records
-    post() {
-        console.log("post");
-    }
-
-    //used to update existing records
-    put() {
-        console.log("put");
-    }
-
-    //used to delete records
-    delete() {
-        console.log("delete");
-    }
-}
+  }
+};
