207d3580e5b5a07e9847f15a11501f4a568dbe59
Brandon Chan <brandonchan45@gmail.com>
2024-02-20 15:05:32 -0800

    added test cases, refactored routes/users.js, removed calendar.js and contact.js routes, depreciated

--- routes/users.js
+++ routes/users.js
@@ -1,6 +1,7 @@
-const express = require('express');
+const express = require("express");
 const router = express.Router();
-const ApiRequests = require('../services/api_requests');
+const ApiRequests = require("../services/api_requests");
+const api = require("../api");
 
 /**
  * Route to make sure the API db is storing the user
@@ -11,50 +12,41 @@ const ApiRequests = require('../services/api_requests');
  * @return {Object} list of all users
  */
 router.get("/users", (req, res) => {
-
-    const api_requests = new ApiRequests("users", req.headers.authorization);
-    let data = api_requests.get();
-    data.then((data) => {  // data is the response from the API
-        console.log(data);
-    })
-        .catch((error) => {
-            console.log(error);
-        });
-
-    res.status(200).json({ users: "users" });
+  api.all(req, res);
+  this.route.res.status(200).json({ users: "users" });
 });
 
+router.param("user_id", function (req, res, next, id) {
+  // sample user, would actually fetch from API
+  req.user = {
+    id: id,
+    name: "TJ",
+  };
+  next();
+});
 
-router.param('user_id', function (req, res, next, id) {
-    // sample user, would actually fetch from API
-    req.user = {
-        id: id,
-        name: 'TJ'
-    }
-    next()
-})
-
-router.route('/user/:user_id')
-    .all(function (req, res, next) {
-        // runs for all HTTP verbs first
-        // think of it as route specific middleware!
-        next()
-    })
-    //get user
-    .get(function (req, res, next) {
-        res.json(req.user)
-    })
-    .put(function (req, res, next) {
-        // just an example of maybe updating the user
-        req.user.name = req.params.name
-        // save user ... etc
-        res.json(req.user)
-    })
-    .post(function (req, res, next) {
-        next(new Error('not implemented'))
-    })
-    .delete(function (req, res, next) {
-        next(new Error('not implemented'))
-    })
+router
+  .route("/user/:user_id")
+  .all(function (req, res, next) {
+    // runs for all HTTP verbs first
+    // think of it as route specific middleware!
+    next();
+  })
+  //get user
+  .get(function (req, res, next) {
+    res.json(req.user);
+  })
+  .put(function (req, res, next) {
+    // just an example of maybe updating the user
+    req.user.name = req.params.name;
+    // save user ... etc
+    res.json(req.user);
+  })
+  .post(function (req, res, next) {
+    next(new Error("not implemented"));
+  })
+  .delete(function (req, res, next) {
+    next(new Error("not implemented"));
+  });
 
 module.exports = router;
