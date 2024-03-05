534463592ac703bf2d0f0d8623771cbc00844f88
Stanley Chow <sjchow97@gmail.com>
2024-03-04 18:25:34 -0800

    refactored CalendarPage.js into a jsx component, styled mobile view to match wireframes

--- client/src/components/Calendar/CalendarComponent.jsx
+++ client/src/components/Calendar/CalendarComponent.jsx
@@ -12,38 +12,8 @@ import {
   useMediaQuery,
   useTheme
 } from '@mui/material';
+import '../../styles/Calendar.css';
 
-// const Calendar = new Calendar(calendarEl, {
-//   // plugins: [dayGridPlugin],
-//   // initialView: "dayGridWeek",
-//   // headerToolbar: {
-//   //   left: "prev,next",
-//   //   center: "title",
-//   //   right: "dayGridWeek,dayGridDay", // user can switch between the two
-//   // },
-
-
-//   plugins: [dayGridPlugin, timeGridPlugin, listPlugin, interactionPlugin],
-//   headerToolbar: {
-//     left: 'prev,next today',
-//     center: 'title',
-//     right: 'dayGridMonth,timeGridWeek,timeGridDay listWeek'
-//   },
-//   initialView: "timeGridWeek",
-//   weekends: 'true',
-//   editable: 'true',
-//   selectable: 'true',
-//   selectMirror: 'true',
-//   dayMaxEvents: 'true',
-//   //events={event}
-//   eventClick: handleEventClick,
-//   select: handleDateSelect,
-//   nowIndicator: 'true',
-//   slotMinTime: "08:00:00",
-//   slotMaxTime: "17:00:00"
-  
-
-// });
 
 let event = [
   {
@@ -84,59 +54,48 @@ function CalendarComponent () {
   const theme = useTheme();
   const isMobile = useMediaQuery(theme.breakpoints.down('sm'));
 
+  const toolbarChunkStyles = {
+    display: 'flex',
+    flexDirection: isMobile ? 'column' : 'row',
+    justifyContent: 'center',
+    alignItems: 'center'
+  };
+
+
   return(
 
-      <Container maxWidth="lg" sx={{ mt: 4, mb: 4 }}>
+    <Container maxWidth="lg" sx={{ mt: 4, mb: 4 }}>
       <Paper elevation={3} sx={{ p: 2, boxShadow: 1, borderRadius: 2 }}>
       
-      <Grid container spacing={isMobile ? 2 : 3}>
+        <Grid container spacing={isMobile ? 2 : 4}>
       
-      <Grid item xs={12}>
+          <Grid item xs={12}>
 
-      <FullCalendar
-        plugins={[dayGridPlugin, timeGridPlugin, listPlugin, interactionPlugin]}
-        headerToolbar={{
-          left: 'today',
-          center: 'prev title next',
-          right: 'dayGridMonth,timeGridWeek,timeGridDay,listWeek'
-        }}
-        sx={{
-          '& .fc-toolbar.fc-header-toolbar': {
-            // Apply styles directly to the center portion of the headerToolbar
-            // For example:
-            backgroundColor: 'lightblue',
-            borderRadius: '5px',
-            padding: '5px',
-            display: 'flex',
-            flexDirection: 'row',
-          }
-        }}
-          
-        initialView="timeGridWeek"
-        weekends={true}
-        editable={true}
-        selectable={true}
-        selectMirror={true}
-        dayMaxEvents={true}
-        events={event}
-        eventClick={handleEventClick}
-        select={handleDateSelect}
-        nowIndicator={true}
-        slotMinTime="08:00:00"
-        slotMaxTime="17:00:00"
-      />
-
-</Grid>
-
-      </Grid>
-      
-
+            <FullCalendar
+              plugins={[dayGridPlugin, timeGridPlugin, listPlugin, interactionPlugin]}
+              headerToolbar={{
+                left: 'today',
+                center: 'prev title next',
+                  right: 'dayGridMonth,timeGridWeek,timeGridDay,listWeek'
+              }}
+              initialView="timeGridWeek"
+              weekends={true}
+              editable={true}
+              selectable={true}
+              selectMirror={true}
+              dayMaxEvents={true}
+              events={event}
+              eventClick={handleEventClick}
+              select={handleDateSelect}
+              nowIndicator={true}
+              slotMinTime="08:00:00"
+              slotMaxTime="17:00:00"
+            />
+          </Grid>
+        </Grid>
       </Paper>
     </Container>
-    
   )
-  
-
 };
 
 
