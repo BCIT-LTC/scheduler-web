7a75688559b70178d66d3f4ebe896513b150e908
Brandon Chan <brandonchan45@gmail.com>
2024-03-05 11:54:13 -0800

    updated test cases for no auth token, and invalid path

--- services/api_requests.test.js
+++ services/api_requests.test.js
@@ -1,56 +1,33 @@
-const ApiRequests = require('./api_requests');
+const ApiRequests = require("./api_requests");
 
-// describe('ApiRequests', () => {
-//   let apiRequests;
-
-//   beforeEach(() => {
-//     apiRequests = new ApiRequests("https://scheduler-api.ltc.bcit.ca/api/", "auth_token");
-//   });
-
-//   afterEach(() => {
-//     jest.restoreAllMocks();
-//   });
-
-//   it('should make a GET request and return the response data', async () => {
-//     // Mock the fetch function
-//     global.fetch = jest.fn().mockResolvedValue({
-//       json: jest.fn().mockResolvedValue({ message: 'Success' }),
-//     });
-
-//     const response = await apiRequests.get();
-
-//     expect(fetch).toHaveBeenCalledWith(apiRequests.url, apiRequests.fetchoptions);
-//     expect(response).toEqual({ message: 'Success' });
-//   });
-
-//   it('should handle errors and return the error object', async () => {
-//     // Mock the fetch function to throw an error
-//     global.fetch = jest.fn().mockRejectedValue(new Error('Network error'));
-
-//     const response = await apiRequests.get();
-
-//     expect(fetch).toHaveBeenCalledWith(apiRequests.url, apiRequests.fetchoptions);
-//     expect(response).toBeInstanceOf(Error);
-//     expect(response.message).toBe('Network error');
-//   });
-// });
-
-describe('ApiRequests', () => {
-
+describe("ApiRequests", () => {
   let apiRequests;
 
-  const authToken = "Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiIxMjM0NTY3ODkwIiwibmFtZSI6IkpvaG4gRG9lIiwiaWF0IjoxNTE2MjM5MDIyfQ.3KXyQxUHeZCWw7L4Knz0rRrJAFrsugYyMU0zD11oQ2w"
+  const authToken =
+    "Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiIxMjM0NTY3ODkwIiwibmFtZSI6IkpvaG4gRG9lIiwiaWF0IjoxNTE2MjM5MDIyfQ.3KXyQxUHeZCWw7L4Knz0rRrJAFrsugYyMU0zD11oQ2w";
 
-  beforeEach(() => {
-        // apiRequests = new ApiRequests("https://scheduler-api.ltc.bcit.ca/api/", authToken);
-    apiRequests = new ApiRequests("http://localhost:8000/api/events/week/3", authToken);
+  it("should throw an error if no authToken is provided", () => {
+    expect(() => {
+      apiRequests = new ApiRequests(
+        "https://scheduler-api.ltc.bcit.ca/api/",
+        undefined
+      );
+    }).toThrow("ApiRequest auth_token is undefined");
   });
 
-  it('should make a GET request and return the response data', async () => {
+  it("should throw an error if no path is provided", () => {
+    expect(() => {
+      apiRequests = new ApiRequests(undefined, authToken);
+    }).toThrow("ApiRequests path is undefined");
+  });
 
+  it("should make a GET request and return the response data", async () => {
+    apiRequests = new ApiRequests(
+      "https://scheduler-api.ltc.bcit.ca/api/events/",
+      authToken
+    );
     const response = await apiRequests.all();
 
-    expect(response).toEqual({ version: '0.0.0' });
-
+    expect(response).toEqual({ version: "0.0.0" });
   });
 });
