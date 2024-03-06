658c5e103697c98ccb0f072a55220d0664e2df28
Brandon Chan <brandonchan45@gmail.com>
2024-03-06 11:14:28 -0800

    changed role to saml_role in check_authorization and passport

--- routes/check_authorization.js
+++ routes/check_authorization.js
@@ -7,8 +7,8 @@
  * express module
  * @const
  */
-const express = require('express');
-const { connect } = require('./indexRoute');
+const express = require("express");
+const { connect } = require("./indexRoute");
 const jwt = require("jsonwebtoken");
 
 /**
@@ -28,60 +28,74 @@ const router = express.Router();
  * @return {Object} boolean of if the user is admin
  */
 router.post("", async (req, res, next) => {
+  let response;
+  const url = new URL(process.env.API_URL + "authorize");
+  try {
+    response = await fetch(url, {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+        Authorization: req.headers.authorization,
+      },
+    });
 
-    let response;
-    const url = new URL(process.env.API_URL + "authorize");
-    try {
-        response = await fetch(url, {
-            method: "POST",
-            headers: {
-                'Content-Type': 'application/json',
-                'Authorization': req.headers.authorization,
-            }
-        });
+    response
+      .json()
+      .then((data) => {
+        if (response.status === 200) {
+          let email = data.email;
+          let first_name = data.first_name;
+          let last_name = data.last_name;
+          let saml_role = data.role;
+          let school = data.school;
+          let program = data.program;
+          let jwtToken = jwt.sign(
+            {
+              email,
+              first_name,
+              last_name,
+              saml_role,
+              app_role,
+              school,
+              program,
+              authorization_checked: true,
+              is_logged_in: true,
+            },
+            process.env.JWT_AUTH_SIGNING_KEY
+          );
 
-        response.json().then((data) => {
-            if (response.status === 200) {
-                let email = data.email
-                let first_name = data.first_name
-                let last_name = data.last_name
-                let role = data.role
-                let school = data.school
-                let program = data.program
-                let jwtToken = jwt.sign({
-                    email,
-                    first_name,
-                    last_name,
-                    role,
-                    school,
-                    program,
-                    authorization_checked: true,
-                    is_logged_in: true
-                }, process.env.JWT_AUTH_SIGNING_KEY);
-
-                res.cookie('jwt', jwtToken, { httpOnly: false, sameSite: 'none', secure: true });
-                res.redirect('/');
-            }
-            else {
-                throw {
-                    http_code: response.status,
-                    msg: data.error,
-                };
-            }
-        }).catch((error) => {
-            switch (error.http_code) {
-                case 400:
-                    return res.status(400).json({ error: "Bad request sent to API: " + error.msg });
-                case 500:
-                    return res.status(500).json({ error: "API cannot perform the request: " + error.msg });
-                default:
-                    return res.status(400).json({ error: "Unknown error: " + error.msg });
-            }
-        });
-    } catch (error) {
-        return res.status(500).json({ error: "API unreachable: " + error.message });
-    }
+          res.cookie("jwt", jwtToken, {
+            httpOnly: false,
+            sameSite: "none",
+            secure: true,
+          });
+          res.redirect("/");
+        } else {
+          throw {
+            http_code: response.status,
+            msg: data.error,
+          };
+        }
+      })
+      .catch((error) => {
+        switch (error.http_code) {
+          case 400:
+            return res
+              .status(400)
+              .json({ error: "Bad request sent to API: " + error.msg });
+          case 500:
+            return res
+              .status(500)
+              .json({ error: "API cannot perform the request: " + error.msg });
+          default:
+            return res
+              .status(400)
+              .json({ error: "Unknown error: " + error.msg });
+        }
+      });
+  } catch (error) {
+    return res.status(500).json({ error: "API unreachable: " + error.message });
+  }
 });
 
 module.exports = router;
-
