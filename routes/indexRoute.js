f0a4aa1b6f55af3b9d42b1ef3ad47f6145dc586e
Viresh Soedhwa <vsoedhwa@bcit.ca>
2024-03-11 11:17:35 -0700

    move serving of index after setting cookie

--- routes/indexRoute.js
+++ routes/indexRoute.js
@@ -17,19 +17,20 @@ const jwt = require("jsonwebtoken");
  * @returns {Object} - The client application HTML file with a default JWT token in a cookie
  */
 router.get('*', (req, res) => {
-  // Resolves the absolute path to the client application's HTML file
-  res.sendFile(path.resolve(__dirname, '../client/build/index.html'));
 
   try {
     let default_jwt_token = jwt.sign({
-        authorization_checked: false,
-        is_logged_in: false
+      authorization_checked: false,
+      is_logged_in: false
     }, process.env.JWT_AUTH_SIGNING_KEY);
     
     res.cookie('default_jwt', default_jwt_token, { httpOnly: false, sameSite: 'none', secure: true });
   } catch (error) {
     return res.status(500).json({ error: "Error retrieving anonymous token: " + error.message });
   }
+
+  // Resolves the absolute path to the client application's HTML file
+  res.sendFile(path.resolve(__dirname, '../client/build/index.html'));
 });
 
 module.exports = router;
